0cde5fb1b043b25c8c145f05e92d174929fb0b7a
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-11 12:31:37 -0500

    Basic introduction to import/export in common and ES2015.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
-const greeting = 'Hello World';
+// common JS module import
+// const person = require('./mymodule1');
+
+// ES2015 module import
+// import { person, sayHello } from './mymodule2';
+
+// grabbing everything in a module
+// import * as mod from './mymodule2';
+
+import greeting from './mymodule2'
 console.log(greeting);
 
-const getData = async (url) => {
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
-};
+// now we can access the object in the mymodule file
+// console.log(mod.person.name);
+// console.log(mod.sayHello());
 
-getData('https://jsonplaceholder.typicode.com/posts');
+// const getData = async (url) => {
+//   const response = await fetch(url);
+//   const result = await response.json();
+//   console.log(result);
+// };
+//
+// getData('https://jsonplaceholder.typicode.com/posts');
---
+++ src/mymodule1.js
@@ -0,0 +1,5 @@
+// .exports means whatever I put here should be available to other files
+module.exports = {
+	name: 'Charles',
+	email: 'charles@fakeemails.com'
+}
